d2ab4816743f5d40bbe1c0153712f284d5d325f0
liuxin <liuxin85@gmail.com>
2023-08-12 16:06:55 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+  </main>
+</template>
